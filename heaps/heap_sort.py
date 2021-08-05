e2ab3d32d5a6c5416b539a05ec1cfc9ae975c3ba
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-05 13:27:55 -0700

    heapsort solution

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,18 @@
+from heaps.min_heap import MinHeap
 
-
-def heap_sort(list):
+def heap_sort(nums):
     """ This method uses a heap to sort an array.
         Time Complexity:  ?
         Space Complexity: ?
     """
-    pass
+    heap = MinHeap()
+
+    for num in nums:
+        heap.add(num)
+
+    index = 0
+    while not heap.empty():
+        nums[index] = heap.remove()
+        index += 1
+
+    return nums

732677c3472a952c2112e283148cd91bbcc16654
rpeeples <rpeeples@anaconda.com>
2022-09-28 12:00:02 -0400

    heaps

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,17 @@
+from heaps.min_heap import MinHeap
 
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O(n log n)
+        Space Complexity: O(1)
     """
-    pass
+    heap = MinHeap()
+    for value in list: 
+        heap.add(list)
+    index = 0
+    while not heap.empty():
+        value[index] = heap.remove()
+        index += 1
+    return value
+    
